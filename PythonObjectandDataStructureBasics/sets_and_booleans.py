9343abcad0bb9401b6d049c73e555fa3d708f544
Raihan Uddin <blackedition.me@gmail.com>
2017-04-26 23:37:28 +0600

    working with sets & booleans

---
+++ PythonObjectandDataStructureBasics/sets_and_booleans.py
@@ -0,0 +1,26 @@
+x = set()
+print(type(x))
+
+x.add(1)
+print(x)
+
+x.add(2)
+print(x)
+
+x.add(1)
+print(x)
+
+l = [1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4]
+print(set(l))
+
+# Booleans
+a = True
+print(a)
+print(type(a))
+print(11 > 2)
+
+b = None
+print(b)
+print(type(b))
+b = 'a'
+print(b)
