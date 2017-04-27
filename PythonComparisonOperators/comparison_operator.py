72212a19359f4f41d11657b9a5129efbd288b8b8
Raihan Uddin <blackedition.me@gmail.com>
2017-04-27 08:06:11 +0600

    working with comparison operator

---
+++ PythonComparisonOperators/comparison_operator.py
@@ -0,0 +1,21 @@
+print(2 == 2)
+
+print(1 == 0)
+print(1 == 1.0)
+
+# not equal
+
+print(2 != 1)
+print(2 != 1)
+
+print(2 > 1)
+print(2 > 4)
+
+print(2 < 4)
+print(2 < 1)
+
+print(2 >= 2)
+print(2 >= 1)
+
+print(2 <= 2)
+print(2 <= 10)
